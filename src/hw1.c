49f6a5b0b31f7f74b3d7b2b59bbf760d4ae337b7
dylanNicastro <145889356+dylanNicastro@users.noreply.github.com>
2024-02-14 18:19:04 +0000

    Finished reconstruct function

--- src/hw1.c
+++ src/hw1.c
@@ -66,25 +66,24 @@ unsigned int compute_checksum_sf(unsigned char packet[])
 }
 
 unsigned int reconstruct_array_sf(unsigned char *packets[], unsigned int packets_len, int *array, unsigned int array_len) {
-    printf("Size: %d\n",packets_len);
+    int success = 0;
     for (int i = 0; i < packets_len; i++) {
         unsigned char **currentPacket = (packets + i);
-
         int packetSize = getPacketLength(*currentPacket);
-        printf("Packet length of packet %d: %d\n", i, packetSize);
-
-        printf("10th hex of packet: %d\n", *((*currentPacket) + 9));
-
         int startIndex = getFragmentOffset(*currentPacket)/4;
-        printf("Integer offset: %d\n", startIndex);
 
-        
         for (int j = 16; j < packetSize; j = j + 4) {
-            unsigned int payload = (*((*currentPacket) + j) << 24) | (*((*currentPacket) + j + 1) << 16) | (*((*currentPacket) + j + 2) << 8) | *((*currentPacket) + j + 3);
-            printf("Payload Integer %d: %d\n", (j-16)/4, payload);
+            if (compute_checksum_sf(*currentPacket) == getChecksum(*currentPacket)) {
+                unsigned int payload = (*((*currentPacket) + j) << 24) | (*((*currentPacket) + j + 1) << 16) | (*((*currentPacket) + j + 2) << 8) | *((*currentPacket) + j + 3);
+
+                if (startIndex + (j - 16)/4 < array_len) {
+                    *(array + startIndex + (j - 16)/4) = payload;
+                    success++;
+                }
+            }
         }
-        printf("\n");
     }
+    return success;
 }
 
 unsigned int packetize_array_sf(int *array, unsigned int array_len, unsigned char *packets[], unsigned int packets_len,
