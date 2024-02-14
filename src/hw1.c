dde2472f6ff65ad2633882237ef0dd7c0bfd43f7
dylanNicastro <145889356+dylanNicastro@users.noreply.github.com>
2024-02-14 04:44:30 +0000

    Print function completed

--- src/hw1.c
+++ src/hw1.c
@@ -1,8 +1,58 @@
 #include "hw1.h"
+#include <math.h>
+
+int getSourceAddress(unsigned char packet[]) {
+    int sourceAddress = (packet[0] << 20) | (packet[1] << 12) | (packet[2] << 4) | (packet[3] >> 4);
+    return sourceAddress;
+}
+int getDestAddress(unsigned char packet[]) {
+    int destAddress = ((packet[3] & 0x0f) << 24) | (packet[4] << 16) | (packet[5] << 8) | packet[6];
+    return destAddress;
+}
+char getSourcePort(unsigned char packet[]) {
+    char sourcePort = packet[7] >> 4;
+    return sourcePort;
+}
+char getDestPort(unsigned char packet[]) {
+    char destPort = packet[7] & 0x0f;
+    return destPort;
+}
+int getFragmentOffset(unsigned char packet[]) {
+    int fragmentOffset = (packet[8] << 6) | (packet[9] >> 2);
+    return fragmentOffset;
+}
+int getPacketLength(unsigned char packet[]) {
+    int packetLength = ((packet[9] & 0x03) << 12) | (packet[10] << 4) | (packet[11] >> 4);
+    return packetLength;
+}
+char getMaxHopCount(unsigned char packet[]) {
+    char maxHopCount = ((packet[11] & 0x0f) << 1) | (packet[12] >> 7);
+    return maxHopCount;
+}
+int getChecksum(unsigned char packet[]) {
+    int checksum = ((packet[12] & 0x7f) << 16) | (packet[13] << 8) | packet[14];
+    return checksum;
+}
+char getCompScheme(unsigned char packet[]) {
+    char compScheme = packet[15] >> 6;
+    return compScheme;
+}
+char getTrafficClass(unsigned char packet[]) {
+    char trafficClass = packet[15] & 0x3f;
+    return trafficClass;
+}
 
 void print_packet_sf(unsigned char packet[])
 {
-    (void)packet;
+    int packetSize = getPacketLength(packet);
+    signed int currentPayload;
+    printf("Source Address: %d\nDestination Address: %d\nSource Port: %d\nDestination Port: %d\nFragment Offset: %d\nPacket Length: %d\nMaximum Hop Count: %d\nChecksum: %d\nCompression Scheme: %d\nTraffic Class: %d\nPayload:",
+            getSourceAddress(packet),getDestAddress(packet),getSourcePort(packet),getDestPort(packet),getFragmentOffset(packet),getPacketLength(packet),getMaxHopCount(packet),getChecksum(packet),getCompScheme(packet),getTrafficClass(packet));
+    for (int i = 16; i < packetSize; i = i + 4) {
+        currentPayload = (packet[i] << 24) | (packet[i+1] << 16) | (packet[i+2] << 8) | packet[i+3];
+        printf(" %d", currentPayload);
+    }
+    printf("\n");
 }
 
 unsigned int compute_checksum_sf(unsigned char packet[])
