1f5d29e56128b9470d2fb3686204cca057664fde
dylanNicastro <145889356+dylanNicastro@users.noreply.github.com>
2024-02-17 05:24:17 +0000

    Trying to fix segfaults

--- src/hw1.c
+++ src/hw1.c
@@ -94,42 +94,44 @@ unsigned int packetize_array_sf(int *array, unsigned int array_len, unsigned cha
     int packets_created = 0;
     int k = 0;
     for (int i = 0; i < packets_len; i++) {
-        int packet_payload_size;
-        if ((i == packets_len - 1) && (array_len < packets_len*(max_payload/4))) {
-            packet_payload_size = array_len*4 - (packets_len-1)*(max_payload);
+        if(array_len-1 < (i)*(max_payload)/4) {
+            int packet_payload_size;
+            if ((i == packets_len - 1) && (array_len < packets_len*(max_payload/4))) {
+                packet_payload_size = array_len*4 - (packets_len-1)*(max_payload);
+            }
+            else {
+                packet_payload_size = max_payload;
+            }
+            int packet_length = 16+packet_payload_size;
+            int fragment_offset = i*max_payload;
+            packets[i] = malloc(packet_length);
+            packets[i][0] = src_addr >> 20;
+            packets[i][1] = (src_addr >> 12) & 0xff;
+            packets[i][2] = (src_addr >> 4) & 0xff;
+            packets[i][3] = ((src_addr & 0x0f) << 4) | (dest_addr >> 24);
+            packets[i][4] = (dest_addr >> 16) & 0xff;
+            packets[i][5] = (dest_addr >> 8) & 0xff;
+            packets[i][6] = dest_addr & 0xff;
+            packets[i][7] = (src_port << 4) | dest_port;
+            packets[i][8] = fragment_offset >> 6;
+            packets[i][9] = ((fragment_offset & 0x3f) << 2) | (packet_length >> 12);
+            packets[i][10] = (packet_length >> 4) & 0xff;
+            packets[i][11] = ((packet_length & 0x0f) << 4) | (maximum_hop_count >> 1);
+            packets[i][12] = ((maximum_hop_count & 0x01) << 7);
+            packets[i][15] = ((compression_scheme & 0x03) << 6) | traffic_class;
+            for (int j = 16; j < packet_length; j = j + 4) {
+                packets[i][j] = array[k] >> 24;
+                packets[i][j+1] = (array[k] >> 16) & 0xff;
+                packets[i][j+2] = (array[k] >> 8) & 0xff;
+                packets[i][j+3] = array[k] & 0xff;
+                k++;
+            }
+            packets_created++;
+            unsigned int checksum = compute_checksum_sf(packets[i]);
+            packets[i][12] = packets[i][12] | (checksum >> 16);
+            packets[i][13] = (checksum >> 8) & 0xff;
+            packets[i][14] = checksum & 0xff;
         }
-        else {
-            packet_payload_size = max_payload;
-        }
-        int packet_length = 16+packet_payload_size;
-        int fragment_offset = i*max_payload;
-        packets[i] = malloc(packet_length);
-        packets[i][0] = src_addr >> 20;
-        packets[i][1] = (src_addr >> 12) & 0xff;
-        packets[i][2] = (src_addr >> 4) & 0xff;
-        packets[i][3] = ((src_addr & 0x0f) << 4) | (dest_addr >> 24);
-        packets[i][4] = (dest_addr >> 16) & 0xff;
-        packets[i][5] = (dest_addr >> 8) & 0xff;
-        packets[i][6] = dest_addr & 0xff;
-        packets[i][7] = (src_port << 4) | dest_port;
-        packets[i][8] = fragment_offset >> 6;
-        packets[i][9] = ((fragment_offset & 0x3f) << 2) | (packet_length >> 12);
-        packets[i][10] = (packet_length >> 4) & 0xff;
-        packets[i][11] = ((packet_length & 0x0f) << 4) | (maximum_hop_count >> 1);
-        packets[i][12] = ((maximum_hop_count & 0x01) << 7);
-        packets[i][15] = ((compression_scheme & 0x03) << 6) | traffic_class;
-        for (int j = 16; j < packet_length; j = j + 4) {
-            packets[i][j] = array[k] >> 24;
-            packets[i][j+1] = (array[k] >> 16) & 0xff;
-            packets[i][j+2] = (array[k] >> 8) & 0xff;
-            packets[i][j+3] = array[k] & 0xff;
-            k++;
-        }
-        packets_created++;
-        unsigned int checksum = compute_checksum_sf(packets[i]);
-        packets[i][12] = packets[i][12] | (checksum >> 16);
-        packets[i][13] = (checksum >> 8) & 0xff;
-        packets[i][14] = checksum & 0xff;
     }
     return packets_created;
 }
